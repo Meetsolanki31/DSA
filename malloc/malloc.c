cdd457d339d7e090aabd434d69b48ecce9c4edab
Meetsolanki31 <133140593+Meetsolanki31@users.noreply.github.com>
2024-10-19 14:07:07 +0530

    Add files via upload

---
+++ malloc/malloc.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[]) {
+	
+	int* ptr;
+	int n,i;
+
+	printf("Enter the size of array you want to create:\n");
+	scanf("%d",&n);
+	
+	ptr = (int *) malloc(n * sizeof(int));
+	for(i=0; i<n; i++)
+	{
+		printf("Enter the value no %d of this array\n",i);
+		scanf("%d",&ptr[i]);
+	}
+	
+	for(i=0; i<n; i++)
+	{
+		printf("the value of %d of this array is %d \n",i,ptr[i]);
+	}
+	free(ptr);
+	return 0;
+}
